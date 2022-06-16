df0cbe8f188079535c43fdbcc5a172c9de35919f
vickyadhi <vickyadhi011@gmail.com>
2022-06-16 16:34:37 +0530

    null changed to abc

--- MyApplication1997/app/src/main/java/com/example/myapplication1997/FragmentMainActivity2.kt
+++ MyApplication1997/app/src/main/java/com/example/myapplication1997/FragmentMainActivity2.kt
@@ -13,7 +13,7 @@ class FragmentMainActivity2 : AppCompatActivity() {
     fun loadfragment(fragment: Fragment){
         val transaction =supportFragmentManager.beginTransaction()
         transaction.replace(R.id.frgmentView, fragment)
-        transaction.addToBackStack(null)
+        transaction.addToBackStack("abc")
         transaction.commit()
 
     }
